8aad712a284584eb3293b45bf8411daa26e415d4
fernandogestomoreno <31586204+fernandogestomoreno@users.noreply.github.com>
2017-09-03 12:51:50 +0100

    Update secretinfo.php

--- secretinfo.php
+++ secretinfo.php
@@ -9,12 +9,14 @@
 <body>
 
 <?php
-	
+	/* the sessions are used in the page to make it secret, you can not access to it by the url without being logged in */
 	if (!empty($_SESSION['uid'])){
 		echo 'Logged in as user '.$_SESSION['un'];
 		echo '<br> HERE YOU CAN SEE THE SECRET INFO, THANK YOU FOR LOGGING IN';
 		echo '<a class="logoutbutton" href="logout.php"> LOG OUT </a>';
 	}
+	/* one of the echoes is a log out button that takes you to the log in page again*/
+	
 	else {
 		echo 'Not logged in...';
 	}
